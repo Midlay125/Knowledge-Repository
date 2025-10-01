2e82b5e201d7ff2126b4899963bde7febd2418aa
Midlay125 <s11149001@student.usp.ac.fj>
2025-10-01 17:51:11 +1200

    Add files via upload

---
+++ UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cs241_assignment2.knowledge_repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ *
+ * @author Issac
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+        
+        if(user == null) { throw new UsernameNotFoundException("Could not find user"); }
+        
+        return new MyUserDetails(user);
+    }
+}
